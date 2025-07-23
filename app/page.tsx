184a5345fb95b471e61be739884a83ac27768134
ChengChangming <excelccm@gmail.com>
2025-07-23 09:25:44 +0800

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div className=' text-3xl text-violet-700 font-black'>Welcome to Next.js</div>
+  return (<>
+    <div className=' text-3xl text-violet-700 font-black font-inter'>Welcome to Next.js</div>
+    <div className=' text-3xl text-violet-700 font-black font-space-grotesk'>Welcome to Next.js</div>
+    </>
   )
 }
 
